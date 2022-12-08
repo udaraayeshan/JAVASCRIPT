36fa7594e4f47bc4af8ba839c0e7376323008150
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-08 21:26:11 +0530

    update object

--- 15.Objects/scripts.js
+++ 15.Objects/scripts.js
@@ -7,28 +7,31 @@
     Those values written as name:value pairs called properties or methods.
 */
 
-// --Object Literals--
-// Comma-separated list of name-value pairs wrapped in curly braces.
+// --[1]Object Literals--
+// Comma-separated list of (name-value pairs )wrapped in curly braces.
 var nimal = {
     firstName: 'Nimal',
-    lastName: 'Perera',
+    lastName: 'Perera',   //Single Variables
     age: 24,
+
     isMarried: false,
     job: 'Developer',
+                                    //Arrary
     subjects: ['Java Programming', 'Web Development', 'Graphic Design', 'Database Management'],
+
     degree: {
-        name: 'Software Eng.',
+        name: 'Software Eng.',  //object ekak athle thawa object ekak hadima
         duration: '4 years',
         credits: 120
     },
     projects: [
         {
-            name: 'Ecomerce App',
+            name: 'Ecomerce App',  
             technology: 'Flutter',
             version: 1.1
         },
         {
-            name: 'Music Player',
+            name: 'Music Player',     //Arrary eke athule object
             technology: 'Java',
             version: 1.2
         },
@@ -37,9 +40,11 @@ var nimal = {
             technology: 'C#',
             version: 2.0
         }
+           // me okkama lakshan object eke..
+
     ],
-    yearOfBirth: function(year){
-        return year-this.age;
+    yearOfBirth: function(year){  //Meka method ekak
+        return year-this.age;    //this eka denne me object eke thina valu eka ganna kiyala kiyanne
     }
 }
 
@@ -50,7 +55,8 @@ console.log(nimal['lastName']);
 var x = 'age';
 console.log(nimal[x]);
 console.log(nimal.subjects[2]);
-console.log(nimal['subjects'][3]);
+console.log(nimal['subjects'][3]);     //Data ganna puluwan widi - 1)name.degree.
+                                                          //       2)name.['degree'].
 console.log(nimal.degree.name);
 console.log(nimal['degree'].credits);
 console.log(nimal.projects[1].technology);
@@ -61,7 +67,7 @@ nimal.job = 'Engineer';
 nimal['isMarried'] = true;
 console.log(nimal);
 
-// --new Object syntax--
+// --[2]new Object syntax--s
 var kasun = new Object();
 // Add Properties
 kasun.firstName = 'Kasun';
